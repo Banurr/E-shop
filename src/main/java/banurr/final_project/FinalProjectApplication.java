c4ac2f95a304d6e3e4d9e137e801c0746e9b6047
Banurr <yermagambetovnurbol@gmail.com>
2024-05-10 16:33:01 +0200

    commit using terminal

--- src/main/java/banurr/final_project/FinalProjectApplication.java
+++ src/main/java/banurr/final_project/FinalProjectApplication.java
@@ -2,9 +2,6 @@ package banurr.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class FinalProjectApplication
--- src/main/java/banurr/final_project/controllers/PictureController.java
+++ src/main/java/banurr/final_project/controllers/PictureController.java
@@ -15,6 +15,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @Controller
 public class PictureController
